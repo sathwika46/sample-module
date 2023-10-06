36afcd62ba6821287c4aee4843024d9a96a1fb99
sathwika46 <sathwikareddy46@gmail.com>
2023-10-06 09:17:30 +0530

    workspace update

--- main.tf
+++ main.tf
@@ -2,6 +2,21 @@ provider "aws" {
     region = "ap-south-1"
   
 }
+variable "ami_value" {
+    description = "value for the ami"
+  
+}
+variable "instance_type_value" {
+    description = "value for instance_type"
+    type = map(string)
+
+    default = {
+      "dev" = "t2.micro"
+      "prod" = "t2.small"
+
+      
+    }
+}
 module "ec2_instance" {
     source = "./mudule/ec2_instance"
     ami_value = "ami-0f5ee92e2d63afc18"
